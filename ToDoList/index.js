51126601aed5e2bef52160a947b55560f3ab714e
omar2432 <50786739+omar2432@users.noreply.github.com>
2023-08-24 16:49:20 +0300

    Task List Created

---
+++ ToDoList/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app=express();
+const port=3000;
+var toDoList=[];
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/",(req,res)=>{
+    res.render("index.ejs",{toDoList});
+});
+
+
+app.post("/submit",(req,res)=>{
+    
+    var newTask=req.body["item"];
+    toDoList.push(newTask);
+    res.render("index.ejs",{toDoList});
+});
+
+
+app.listen(port,()=>{
+    console.log(`server running on port ${port}`);
+});
