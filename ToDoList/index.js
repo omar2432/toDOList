c213c89522515c1fb46af3299b4e5c9e3badaaab
omar2432 <50786739+omar2432@users.noreply.github.com>
2023-08-28 17:42:51 +0300

    Add files via upload

--- ToDoList/index.js
+++ ToDoList/index.js
@@ -3,21 +3,38 @@ import bodyParser from "body-parser";
 
 const app=express();
 const port=3000;
+var firstList=true;
 var toDoList=[];
+var otherToDoList=[];
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use('/node_modules', express.static('node_modules'));
+
+//app.use(express.static("public"));
+
+
 
 app.get("/",(req,res)=>{
-    res.render("index.ejs",{toDoList});
+    res.render("index.ejs",{firstList,toDoList,otherToDoList});
 });
 
 
 app.post("/submit",(req,res)=>{
     
     var newTask=req.body["item"];
-    toDoList.push({text:newTask,completed:false});
-    res.render("index.ejs",{toDoList});
+    if(firstList){
+
+        toDoList.push({text:newTask,completed:false});
+    }else{
+        otherToDoList.push({text:newTask,completed:false});
+    }
+    res.render("index.ejs",{firstList,toDoList,otherToDoList});
+});
+
+
+app.post("/change",(req,res)=>{
+    firstList=!firstList;
+    res.render("index.ejs",{firstList,toDoList,otherToDoList});
 });
 
 
